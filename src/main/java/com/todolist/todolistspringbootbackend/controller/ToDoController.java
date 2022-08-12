f40492e3f9d97b4e2910e7884c40999c21e5581f
DulanjayaSandaruwan <dulanjayasandaruwan1998@gmail.com>
2022-08-13 00:23:22 +0530

    add controllers

--- src/main/java/com/todolist/todolistspringbootbackend/controller/ToDoController.java
+++ src/main/java/com/todolist/todolistspringbootbackend/controller/ToDoController.java
@@ -1,8 +1,50 @@
 package com.todolist.todolistspringbootbackend.controller;
 
+import com.todolist.todolistspringbootbackend.dto.ToDoDTO;
+import com.todolist.todolistspringbootbackend.service.ToDoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import java.util.List;
+
 /**
  * @author : D.D.Sandaruwan <dulanjayasandaruwan1998@gmail.com>
  * @Since : 12/08/2022
  **/
+
+@RestController
+@RequestMapping("api/v1/users/{userId:[A-Fa-f0-9\\-]{36}}/todo")
 public class ToDoController {
+
+    @Autowired
+    public ToDoService toDoService;
+
+    public ToDoController(ToDoService toDoService) {
+        this.toDoService = toDoService;
+    }
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = "application/json",produces = "application/json")
+    public ToDoDTO addToDo(@PathVariable String userId, @RequestBody @Valid ToDoDTO toDo, Errors errors){
+        if (errors.hasFieldErrors()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,errors.getFieldErrors().get(0).getDefaultMessage());
+        }
+        if(!userId.equals(toDo.getUserId()))
+            throw new ResponseStatusException(HttpStatus.CONFLICT,"User Id is mismatch");
+        return toDoService.saveToDo(toDo);
+
+    }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping(path = "/{toDoId:\\d+}")
+    public void deleteToDo(@PathVariable String userId,@PathVariable int toDoId){
+        toDoService.deleteToDo(userId, toDoId);
+
+    }
+    @GetMapping(produces = "application/json")
+    public List<ToDoDTO> grtAllNotes(@PathVariable String userId){
+        return toDoService.getAllToDos(userId);
+    }
 }
--- src/main/java/com/todolist/todolistspringbootbackend/controller/UserController.java
+++ src/main/java/com/todolist/todolistspringbootbackend/controller/UserController.java
@@ -1,8 +1,57 @@
 package com.todolist.todolistspringbootbackend.controller;
 
+import com.todolist.todolistspringbootbackend.dto.UserDTO;
+import com.todolist.todolistspringbootbackend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
 /**
  * @author : D.D.Sandaruwan <dulanjayasandaruwan1998@gmail.com>
  * @Since : 12/08/2022
  **/
+
+@RestController
+@RequestMapping("api/v1/users")
 public class UserController {
+
+    @Autowired
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = "application/json",produces = "application/json")
+    public UserDTO registerUser(@RequestBody @Validated UserDTO user, Errors errors){
+        if(errors.hasFieldErrors()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,errors.getFieldErrors()
+                    .get(0).getDefaultMessage());
+        }
+        return userService.registerUser(user);
+
+    }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping(path = "/{userId}",consumes = "application/json")
+    public void updateUser(@PathVariable String userId,@RequestBody UserDTO user, Errors errors){
+        if(errors.hasFieldErrors()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,errors.getFieldErrors()
+                    .get(0).getDefaultMessage());
+        }
+        user.setId(userId);
+        userService.updateUser(user);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping(path = "/{userId:[A-Fa-f0-9\\-]{36}}")
+    public void deleteUser(@PathVariable String userId){
+        userService.deleteUser(userId);
+    }
+    @GetMapping(path = "/{userId:[A-Fa-f0-9\\-]{36}}",produces = "application/json")
+    public UserDTO getUserInfo(@PathVariable String userId){
+        return userService.getUserInfo(userId);
+    }
 }
